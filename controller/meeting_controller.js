03c696a836e6b8468430f6d368b8369fead84440
brijesh165 <brijesh16591@gmail.com>
2020-12-08 12:50:31 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -18,10 +18,8 @@ exports.getMeeting = async (params, cb) => {
                 id: params.meeting_id
             }
         });
-
-        console.log("Meeting : ", meeting!==null, meeting.length);
         
-        if (meeting.length) {
+        if (meeting.length > 0) {
             response = {
                 code: 200,
                 message: "success"
