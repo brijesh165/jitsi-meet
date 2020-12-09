f57bdb9e72e8c9ce9c634a4d17fcdacd329eaf36
brijesh165 <brijesh16591@gmail.com>
2020-12-09 13:04:00 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -18,7 +18,7 @@ exports.getMeeting = async (params, cb) => {
             }
         });
 
-        console.log("Meeting : ", meeting.values(id));
+        console.log("Meeting : ", meeting);
         console.log("Encrypt: ", appUtil.encryptMeetingId(meeting.dataValues.id, "start"));
         console.log("Decrypt: ", appUtil.decryptMeetingId(appUtil.encryptMeetingId(meeting.dataValues.id, "start")));
 
