a2640e95ac44324da7f2546b72af37c0524a285f
brijesh165 <brijesh16591@gmail.com>
2020-12-09 18:59:10 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -74,8 +74,8 @@ exports.createmeeting = async (params, cb) => {
 
 exports.startMeeting = async (params, cb) => {
     try {
-        console.log("Start Meeting Params: ", params);
-        
+        console.log("Start Meeting Params: ", params.params.id);
+
         const queryParams = params.id;
         const meeting_id = appUtil.decryptMeetingId(queryParams).split(" ")[0];
         const userstatus = appUtil.decryptMeetingId(queryParams).split(" ")[1];
