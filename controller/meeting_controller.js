4e37e8b51739a5b915d9fdaa3fb90ae6fbfbed0a
brijesh165 <brijesh16591@gmail.com>
2020-12-10 18:37:23 +0530

    fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -76,9 +76,11 @@ exports.createmeeting = async (params, cb) => {
             console.log("DecriptedMeetingId: ", appUtil.decryptMeetingId(encryptedMeetingforjoin));
             response.start_url = `https://meet.teamlocas.com:3030/join/${encryptedMeetingforstart}`;
             response.join_url = `https://meet.teamlocas.com:3030/join/${encryptedMeetingforjoin}`;
-
+            console.log("Response: ", response);
             return cb(null, appUtil.createSuccessResponse(appUtil.createSuccessResponse(constants.responseCode.SUCCESS, response)));
         } else if (params.meeting_type == 'weekly') {
+            let response = {};
+            
             const createmeetingparams = {
                 application: params.application,
                 meeting_host: params.meeting_host,
