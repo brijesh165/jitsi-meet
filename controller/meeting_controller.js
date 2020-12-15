b6b54f48a6a981becf6dc0c73c22e97df4dd19c1
brijesh165 <brijesh16591@gmail.com>
2020-12-15 14:18:11 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -138,14 +138,14 @@ exports.startMeeting = async (req, res) => {
                         id: meeting.id
                     }
                 });
-                return res.redirect(`https://meet.teamlocus.com/${meeting.id}${meeting_host}`)
+                return res.redirect(`https://meet.teamlocus.com/${meeting.id}_${meeting_host}`)
             } else if (meeting && meeting.meeting_type == "weekly" && moment(meeting.end_time).format("HHmm") > moment().utc().format("HHmm")) {
                 await models.meeting.update({ status: "started", actual_start_time: moment().utc().toDate().valueOf() }, {
                     where: {
                         id: meeting.id
                     }
                 });
-                return res.redirect(`https://meet.teamlocus.com/${meeting.id}${meeting_host}`);
+                return res.redirect(`https://meet.teamlocus.com/${meeting.id}_${meeting_host}`);
             } else {
                 return res.redirect(`https://meet.teamlocus.com/waiting`)
             }
