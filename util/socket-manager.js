e1bac9bf56e00f1b17813abf20f3cf1199f9299c
brijesh165 <brijesh16591@gmail.com>
2020-12-21 13:12:31 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -20,7 +20,7 @@ exports.openIO = function (io) {
     
         })
 
-        console.log(`Socket Connection successful${socket.id}`);
+        console.log(`Socket Connection successful ${socket.id}`);
     })
 }
 
