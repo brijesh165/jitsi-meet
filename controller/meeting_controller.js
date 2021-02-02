7aa5214ed8a5f14c1f858547dae88ffcb9e812be
Bhawana <bhawanags@tasktower.net>
2021-02-02 14:20:46 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -240,8 +240,10 @@ exports.startMeeting = async (req, res) => {
                 if (meeting.repeat_end_date.getTime().valueOf() > moment().utc().toDate().valueOf()) {
                     const day = moment().isoWeekday();
                     const dayFromParams = moment(meeting.start_time).isoWeekday();
-                    const time = moment().utc().format("hh:mm");
-                    const timeFromParams = moment(meeting.start_time).format("hh:mm");
+                    const time = moment().utc().format("hh:mma");
+                    const timeFromParams = moment(meeting.start_time).format("hh:mma");
+                    console.log("1", time.isAfter(timeFromParams));
+                    console.log("2", time.isBefore(timeFromParams))
                     // console.log("Day: " + day + ": Day From Params: " + dayFromParams);
                     // console.log("Time: ", moment(meeting.start_time).format("hh:mm"));
                     // console.log("Current Time: ", moment().utc().format("hh:mm"))
