a0f46386a6d0a058b6ca19c8ce1cb173beb5ef26
Bhawana <bhawanags@tasktower.net>
2021-02-04 17:06:26 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -272,9 +272,9 @@ exports.startMeeting = async (req, res) => {
 
         if (userstatus == "start") {
             if (meeting && meeting.meeting_type == "nonperiodic") {
-                console.log("In 1", meeting.end_time.getTime().valueOf());
+                console.log("In 1", moment(meeting.end_time).valueOf());
                 console.log("In 2", moment().utc().toDate().getTime().valueOf());
-                console.log("In 3", meeting.end_time.getTime().valueOf() > moment().utc().toDate().getTime().valueOf())
+                console.log("In 3", moment(meeting.end_time).valueOf() > moment().utc().toDate().getTime().valueOf())
                 if (moment(meeting.end_time).getTime().valueOf() > moment().utc().toDate().getTime().valueOf()) {
                     await models.meeting.update({ status: "started", actual_start_time: moment().utc().toDate().valueOf() }, {
                         where: {
