398eb613d826e3f7415ddd3027909a6a3221b330
Bhawana <bhawanags@tasktower.net>
2021-02-12 19:48:21 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -283,9 +283,9 @@ function meetingStatusCheck(params) {
                     // console.log(result)
 
                     console.log("1: ", moment().utc().endOf("month").week() + 1);
-                    console.log("2: ", moment(todaysday).startOf('month').week());
+                    console.log("2: ", moment(todaysday).startOf('month').week() - 1);
                     console.log("3: ", moment(1615558065000).endOf("month").week())
-                    let end = moment().utc().endOf("month").week() + 1 - moment(todaysday).startOf('month').week() + 1;
+                    let end = moment().utc().endOf("month").week() - 1 - moment(todaysday).startOf('month').week() + 1;
 
                     let indexofweek = weekno.indexOf(currentweekno.toString());
 
