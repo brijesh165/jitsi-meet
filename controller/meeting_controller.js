1a57e42b0716cd8b8708560ad183ad51219db0f2
Bhawana <bhawanags@tasktower.net>
2021-02-12 19:10:58 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -283,7 +283,7 @@ function meetingStatusCheck(params) {
                     // console.log(result)
 
                     let indexofweek = weekno.indexOf(currentweekno.toString());
-                    let result = days[indexofweek];
+                    let result = days[indexofweek] ==  currentday.toString();
                     console.log(typeof indexofweek + " " + result + " " + typeof result);
 
                 }
