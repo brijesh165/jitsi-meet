b94c7cec46e5feff02cba61aafed3928cfa02644
Bhawana <bhawanags@tasktower.net>
2021-02-16 14:56:56 +0530

    Fix

--- util/schedule-manager.js
+++ util/schedule-manager.js
@@ -2,91 +2,74 @@ const schedule = require('node-schedule');
 const models = require('./../models');
 const { Op } = require('sequelize');
 
-exports.meetingStatusChange = async function (req, res) {
-    try {
+// exports.meetingStatusChange = async function (req, res) {
+//     try {
 
-        // const allstartedmeetings = await models.meeting.findAll({
-        //     where: {
-        //     }
-        // });
-        // for (let i = 0; i < allstartedmeetings.length; i++) {
-        await models.meeting.update({ status: 'ended' }, {
-            where: {
-                status: 'started'
-            }
-        })
-        // }
+//         const allstartedmeetings = await models.meeting.findAll({
+//             where: {
+//             }
+//         });
+//         for (let i = 0; i < allstartedmeetings.length; i++) {
+//             await models.meeting.update({ status: 'ended' }, {
+//                 where: {
+//                     status: 'started'
+//                 }
+//             })
+//         }
 
-        // const allendedmeetings = await models.meeting.findAll({
-        //     where: {
-        //         status: 'ended',
-        //         repeat_end_date: {
-        //             [Op.gt]: new Date()
-        //         }
-        //     }
-        // })
+//         const allendedmeetings = await models.meeting.findAll({
+//             where: {
+//                 status: 'ended',
+//                 repeat_end_date: {
+//                     [Op.gt]: new Date()
+//                 }
+//             }
+//         })
 
-        // for (let i = 0; i < allendedmeetings.length; i++) {
-        await models.meeting.update({ status: 'pending' }, {
-            where: {
-                status: 'ended',
-                repeat_end_date: {
-                    [Op.gt]: new Date()
-                }
-            }
-        })
+//         for (let i = 0; i < allendedmeetings.length; i++) {
+//             await models.meeting.update({ status: 'pending' }, {
+//                 where: {
+//                     status: 'ended',
+//                     repeat_end_date: {
+//                         [Op.gt]: new Date()
+//                     }
+//                 }
+//             })
 
-        return res.send({
-        code: 200,
-        message: "Success"
-    })
-} catch (error) {
-    console.log("Schedule Manager | Meeting Status Change", error);
-    return res.send({
-        code: 501,
-        message: "Internal Server Error."
-    })
-}
-}
+//             return res.send({
+//                 code: 200,
+//                 message: "Success"
+//             })
+//         } catch (error) {
+//             console.log("Schedule Manager | Meeting Status Change", error);
+//             return res.send({
+//                 code: 501,
+//                 message: "Internal Server Error."
+//             })
+//         }
+//     }
 
 // Change meeting status at 12:00
 let meetingstatuschange = schedule.scheduleJob('00 00 * * *', async function () {
-    try {
+        try {
 
-        // Will change all the meetings status from started to ended
-        // const allstartedmeetings = await models.meeting.findAll({
-        //     where: {
-        //         status: 'started'
-        //     }
-        // });
-        // for (let i = 0; i < allstartedmeetings.length; i++) {
-        await models.meeting.update({ status: 'ended' }, {
-            where: {
-                status: 'started'
-            }
-        })
-        // }
-
-        // Will change all the meetings status from ended to pending
-        // const allendedmeetings = await models.meeting.findAll({
-        //     where: {
-        //         status: 'ended',
-        //         repeat_end_date: {
-        //             [Op.gt]: new Date()
-        //         }
-        //     }
-        // })
-        // for (let i = 0; i < allendedmeetings.length; i++) {
-        await models.meeting.update({ status: 'pending' }, {
-            where: {
-                status: 'ended',
-                repeat_end_date: {
-                    [Op.gt]: new Date()
+            // Will change all the meetings status from ended to started
+            await models.meeting.update({ status: 'ended' }, {
+                where: {
+                    status: 'started'
                 }
-            }
-        })
-        // }
-    } catch (error) {
-        console.log("Schedule Manager | Meeting Status Change", error);
-    }
-});
+            })
+
+            // Will change all the meetings status from ended to pending
+            await models.meeting.update({ status: 'pending' }, {
+                where: {
+                    status: 'ended',
+                    repeat_end_date: {
+                        [Op.gt]: new Date()
+                    }
+                }
+            })
+        } catch (error) {
+            console.log("Schedule Manager | Meeting Status Change", error);
+        }
+    });
