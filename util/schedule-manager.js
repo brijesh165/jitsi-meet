3f0a280451b4a7d48cb4667502b3661e9a6e61ed
Bhawana <bhawanags@tasktower.net>
2021-02-16 14:29:28 +0530

    Fix

--- util/schedule-manager.js
+++ util/schedule-manager.js
@@ -5,21 +5,29 @@ const { Op } = require('sequelize');
 exports.meetingStatusChange = async function (req, res) {
     try {
 
-        const allstartedmeetings = await models.meeting.findAll({
+        // const allstartedmeetings = await models.meeting.findAll({
+        //     where: {
+        //     }
+        // });
+        // for (let i = 0; i < allstartedmeetings.length; i++) {
+        await models.meeting.update({ status: 'ended' }, {
             where: {
                 status: 'started'
             }
-        });
+        })
+        // }
 
-        for (let i = 0; i < allstartedmeetings.length; i++) {
-            await models.meeting.update({ status: 'ended' }, {
-                where: {
-                    meeting_id: allstartedmeetings[i].meeting_id
-                }
-            })
-        }
+        // const allendedmeetings = await models.meeting.findAll({
+        //     where: {
+        //         status: 'ended',
+        //         repeat_end_date: {
+        //             [Op.gt]: new Date()
+        //         }
+        //     }
+        // })
 
-        const allendedmeetings = await models.meeting.findAll({
+        // for (let i = 0; i < allendedmeetings.length; i++) {
+        await models.meeting.update({ status: 'pending' }, {
             where: {
                 status: 'ended',
                 repeat_end_date: {
@@ -28,25 +36,17 @@ exports.meetingStatusChange = async function (req, res) {
             }
         })
 
-        for (let i = 0; i < allendedmeetings.length; i++) {
-            await models.meeting.update({ status: 'pending' }, {
-                where: {
-                    meeting_id: allendedmeetings[i].meeting_id
-                }
-            })
-        }
-
         return res.send({
-            code: 200,
-            message: "Success"
-        })
-    } catch (error) {
-        console.log("Schedule Manager | Meeting Status Change", error);
-        return res.send({
-            code: 501,
-            message: "Internal Server Error."
-        })
-    }
+        code: 200,
+        message: "Success"
+    })
+} catch (error) {
+    console.log("Schedule Manager | Meeting Status Change", error);
+    return res.send({
+        code: 501,
+        message: "Internal Server Error."
+    })
+}
 }
 
 // Change meeting status at 12:00
