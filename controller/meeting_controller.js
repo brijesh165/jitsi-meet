30774d51911cc758317708dcb381d957b74060f5
Bhawana <bhawanags@tasktower.net>
2021-02-27 12:29:35 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -413,6 +413,7 @@ exports.startMeeting = async (req, res) => {
 
         console.log("Meeting: ", meeting == null);
         if (meeting == null) {
+            console.log("Condition True");
             return res.redirect(`https://meet.teamlocus.com/errorpage?${meeting.meeting_id}`)
         }
 
