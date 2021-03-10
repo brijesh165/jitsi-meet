56edb99ee5087da2876f973e07d58c3998fdb0d4
Bhawana <bhawanags@tasktower.net>
2021-03-10 17:12:25 +0530

    Fix

--- routes/meeting_route.js
+++ routes/meeting_route.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
   app.post('/create-meeting', [
     check('application').not().isEmpty().isIn(['teamlocus', 'tlchat']).withMessage('Please provide application name or application name should be teamlocus/tlchat'),
     check('meeting_host').not().isEmpty().withMessage('Meeting host is required.'),
-    check('meeting_type').not().isEmpty().isIn(['periodic', 'nonperiodic']).withMessage('Meeting type should be periodic or non-periodic'),
+    check('meeting_type').not().isEmpty().isIn(['periodic', 'nonperiodic', 'onetime']).withMessage('Meeting type should be periodic, non-periodic or onetime'),
     check('subject').not().isEmpty().withMessage("Subject is required."),
     check('start_time').not().isEmpty().withMessage('Start Time is required.'),
     check('end_time').not().isEmpty().withMessage('End Time is required.')
