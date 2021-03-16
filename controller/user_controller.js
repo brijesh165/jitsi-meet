2ad7c62adc84fe92fb40e8a5a6d188314f830c5f
Bhawana <bhawanags@tasktower.net>
2021-03-16 12:28:02 +0530

    Fix

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,7 +1,26 @@
+const appUtil = require('./../util/app-util');
 
+/**
+ * 
+ * @param {*} username 
+ * @param {*} password
+ * @param {*} gmtoffset
+ * @param {*} deviceinfo
+ * @param {*} deviceid
+ * @param {*} locationinfo 
+ */
 exports.login = async (req, res) => {
     try {
         console.log("Login Params: ", req.body);
+        const guidScript = () => {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+    
+        const guid = (guidScript() + guidScript() + "-" + guidScript() + "-4" + guidScript().substr(0, 3) + "-" + guidScript() + "-" + guidScript() + guidScript() + guidScript()).toLowerCase();
+        
+        return res.send({
+            status: 200
+        });
     } catch (err) {
         console.log("User Controller | Login ", err);
     }
