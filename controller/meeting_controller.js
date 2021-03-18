8739cf13fcac619c78898991eedd4fc85c018741
Bhawana <bhawanags@tasktower.net>
2021-03-18 18:58:23 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -9,6 +9,7 @@ exports.getAllMeetings = async (req, res) => {
         console.log("Get All Meetings: ", req.body);
         const meetings = await axios.post("https://dummyservice.teamlocus.com/webservice_v42.svc/calendararea_listvideomeeting", req.body)
                             .then((data) => {
+                                console.log("Resp: ", data.response)
                                 return data.response.tblmymeetings.map(item=>item.meeting_video)
                             })
 
