09adb4e222ba9a3c6f8ae81f1ec224e94594670c
Bhawana <bhawanags@tasktower.net>
2021-03-26 16:42:02 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -23,8 +23,9 @@ exports.getAllMeetings = async (req, res) => {
         const userMeetings = await models.meeting.findAll({
             where: {
                 'meeting_id': [meetings.data.response.tblmymeetings.map(item => item.meeting_video)],
-                'start_time': moment().utc().valueOf(),
-                'end_time': moment().utc().add(1, 'days').valueOf()
+            },
+            [Op.or]: {
+                "application": "jitsi"
             },
             order: [
                 ['start_time', 'ASC']
