7eb703a03f50a5c52a95e090682ad256ace92088
Bhawana <bhawanags@tasktower.net>
2021-04-12 12:13:29 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -334,7 +334,7 @@ exports.createmeeting = async (req, res) => {
 function meetingStatusCheck(params) {
     try {
         // console.log("Params: ", params);
-        const difference = moment().utc().diff(moment(params.start_time), 'days');
+        const difference = moment().startOf('day').diff(moment(params.start_time).startOf('day'), 'days');
         console.log("Difference: ", difference, moment().utc(), moment(params.start_time))
         console.log("Meeting Type: ", params.meeting_type)
         console.log("Repeat Until: ", params.repeat_event_until)
