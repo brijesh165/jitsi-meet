f5d645947183e606adecc1178dc089ddbcdb1450
Bhawana <bhawanags@tasktower.net>
2021-04-24 11:31:29 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -5,8 +5,6 @@ let socketIO;
 
 exports.openIO = function (io) {
     socketIO = io;
-    let isHost;
-    let meetingId;
     
     io.on('connection', function (socket) {
         socket.on("hangup", async (data) => {
@@ -22,21 +20,27 @@ exports.openIO = function (io) {
     
         })
 
-        socket.on("isHost", async (data) => {
-            console.log("IsHost Data: ", data)
-            isHost = data.role;
-            meetingId = data.meeting_id;
+        socket.on("joinMeeting", (data) => {
+            socket.isHost = data.role;
+            socket.meetingId = data.meetingId;
+            socket.join(data.meetingId)
+        })
 
-            console.log("isHost: ", isHost);
-            console.log("meetingId: ", meetingId);
-        });
+        // socket.on("isHost", async (data) => {
+        //     console.log("IsHost Data: ", data)
+        //     isHost = data.role;
+        //     meetingId = data.meeting_id;
+
+        //     console.log("isHost: ", isHost);
+        //     console.log("meetingId: ", meetingId);
+        // });
 
         socket.on("disconnect", () => {
-            console.log("Socket closed", isHost, meetingId);
-            socketIO.emit("end_meeting", {
-                "role": isHost,
-                "meetingId": meetingId
-            })
+            if (socket.isHost === "host") {
+                socketIO.to(socket.meetingId).emit("end_meeting", {
+                    "meetingId": meetingId
+                })
+            }
         });
 
         console.log(`Socket Connection successful ${socket.id}`);
