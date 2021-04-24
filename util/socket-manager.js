271bbc9c5b6845c920d842fb847570f2f5ec978c
Bhawana <bhawanags@tasktower.net>
2021-04-24 11:07:02 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -33,7 +33,7 @@ exports.openIO = function (io) {
 
         socket.on("disconnect", () => {
             console.log("Socket closed", isHost, meetingId);
-            socket.emit("end_meeting", {
+            socketIO.emit("end_meeting", {
                 "role": isHost,
                 "meetingId": meetingId
             })
