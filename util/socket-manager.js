364dc49846da345d68ee778e4e2afdd15e3e27d1
Bhawana <bhawanags@tasktower.net>
2021-04-30 18:58:09 +0530

    FIx:

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -2,10 +2,31 @@ const models = require('./../models');
 const moment = require('moment');
 
 let socketIO;
-let meetingSockets = {};
+
 
 exports.openIO = function (io) {
     socketIO = io;
+    let meetingSockets = {};
+    let endMeeingSocket = [];
+
+    setInterval(() => {
+        if (endMeeingSocket.length > 0) {
+            for (let i = 0; i < endMeeingSocket.length; i++) {
+                if (moment.utc() >= moment(endMeeingSocket[i].disconnectionTime, 'x').add('15', 'seconds')) {
+                    socketIO.to(endMeeingSocket[i].meetingId).emit("end_meeting", {
+                        "meetingId": endMeeingSocket[i].meetingId
+                    })
+
+                    await models.meeting.update({ status: "ended", actual_end_time: moment().utc().toDate().valueOf() }, {
+                        where: {
+                            meeting_id: endMeeingSocket[i].meetingId
+                        }
+                    });
+                    endMeeingSocket.splice(i, 1);
+                }
+            }
+        }
+    }, 1000)
 
     io.on('connection', function (socket) {
         // socket.on("hangup", async (data) => {
@@ -18,35 +39,49 @@ exports.openIO = function (io) {
         //         });
         //         socketIO.emit("end_meeting", { "meeting_id": data.meeting_id });
         //     }
-    
+
         // })
 
         socket.on("joinMeeting", (data) => {
-            console.log("Join meetings: ", data);
+            console.log("Join meetings: ", data, socket.id);
             socket.isHost = data.role;
             socket.meetingId = data.meetingId;
 
             if (data.role === "host") {
                 meetingSockets[data.meetingId] = socket.id;
+
+                // push meeting into into endMeetingSocket array
+                endMeeingSocket = endMeeingSocket.filter(item => {
+                    item.meetingId != data.meetingId
+                })
             }
+
 
             socket.join(data.meetingId)
         })
 
         socket.on("roleChange", (data) => {
+            console.log("Role Change: Meeting Socket: ", data, socket.id, meetingSockets);
             if (data.role === "host") {
                 meetingSockets[data.meetingId] = socket.id;
             }
-            console.log("Role Change: Meeting Socket: ", data, meetingSockets);
             // meetingSockets.push({ isHost: data.role, meetingId: data.meetingId })
         })
+
+
 
         socket.on("disconnect", () => {
             console.log("Disconnect", socket.isHost, socket.id)
             if (meetingSockets[socket.meetingId] == socket.id) {
-                socketIO.to(socket.meetingId).emit("end_meeting", {
-                    "meetingId": socket.meetingId
+                endMeeingSocket.push({
+                    "meetingId": socket.meetingId,
+                    "disconnectionTime": moment.utc()
                 })
+                console.log("End Meeting Socket: ", endMeeingSocket)
+
+                // socketIO.to(socket.meetingId).emit("end_meeting", {
+                //     "meetingId": socket.meetingId
+                // })
             }
         });
 
