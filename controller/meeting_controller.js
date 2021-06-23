d45d04f67642c40a06b4750d82de362c35f401f6
Bhawana <bhawanags@tasktower.net>
2021-06-23 17:08:15 +0530

    Fix

--- controller/meeting_controller.js
+++ controller/meeting_controller.js
@@ -776,7 +776,7 @@ exports.changeMeetingStatus = async (req, res) => {
         }
         else if (req.body.status == "ended") {
             // console.log("Meeting Details: ", meetingDetails)
-            console.log("Ended", meetingDetails.meeting_type);
+            console.log("Ended", meetingDetails);
             if (meetingDetails.meeting_type === "periodic") {
                 console.log("Periodic");
                 const params = {
