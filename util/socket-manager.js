835d30d6ffca29b46e363727e115ad69bc016457
brijesh165 <brijesh16591@gmail.com>
2021-12-09 15:41:16 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -94,7 +94,7 @@ exports.openIO = function (io) {
             //     joinMeetingSocket.push({ meetingId: [username] })
             // }
 
-            console.log(`joinMeetingSocket :`, joinMeetingSocket, joinMeetingSocket.id, joinMeetingSocket.members)
+            console.log(`joinMeetingSocket :`, joinMeetingSocket, joinMeetingSocket.find(item => item === meetingId))
             io.emit("person_waiting", {
                 "meetingId": data.meetingId,
                 "username": data.username,
