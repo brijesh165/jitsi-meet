684459a798a18ce90d8c01cbcf2b22cddd231816
brijesh165 <brijesh16591@gmail.com>
2021-12-14 20:15:19 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -96,14 +96,12 @@ exports.openIO = function (io) {
 
             console.log(`joinMeetingSocket :`, joinMeetingSocket, joinMeetingSocket[meetingId].members, "Socket id", socket.id)
 
-            setInterval(() => {
-                io.emit("person_waiting", {
-                    "meetingId": data.meetingId,
-                    "username": data.username,
-                    "role": "participant",
-                    "userSocketId": socket.socketId,
-                })
-            }, 10000)
+            io.emit("person_waiting", {
+                "meetingId": data.meetingId,
+                "username": data.username,
+                "role": "participant",
+                "userSocketId": socket.socketId,
+            })
         })
 
         socket.on('allowOne', (data) => {
