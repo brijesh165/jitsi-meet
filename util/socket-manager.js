da217e5d9d6ab1cb6606f596a3a2d9db67c4b8b3
brijesh165 <brijesh16591@gmail.com>
2022-01-05 11:42:30 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -152,8 +152,8 @@ exports.openIO = function (io) {
         socket.on("disconnect", () => {
             console.log("Disconnect", socket.isHost, meetingSockets, socket.meetingId, socket.id)
 
-            if (socket.isHost != "host" && socket.meetingId && joinMeetingSocket[socket.meetingId].members.length > 0) {
-                const afterremoverdParticipant = joinMeetingSocket[socket.meetingId].members.filter((item) => item.id !== socket.id);
+            if (socket.isHost != "host" && socket.meetingId) {
+                const afterremoverdParticipant = joinMeetingSocket[socket.meetingId].members.length > 0 && joinMeetingSocket[socket.meetingId].members.filter((item) => item.id !== socket.id);
                 console.log("Removed Participant: ", afterremoverdParticipant);
 
                 joinMeetingSocket[socket.meetingId].members = afterremoverdParticipant;
