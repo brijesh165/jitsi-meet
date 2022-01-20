b00a815af43066c2de317ae9eb667eb08c7796c8
brijesh165 <brijesh16591@gmail.com>
2022-01-20 12:07:40 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -150,6 +150,8 @@ exports.openIO = function (io) {
 
             if (data.allow_all && joinMeetingSocket[data.meetingId].members.length > 0) {
                 for (let i = 0; i < joinMeetingSocket[data.meetingId].members.length; i++) {
+                    joinMeetingSocket[data.meetingId].members[i].allowed = true;
+                    console.log('Allowed All :', joinMeetingSocket[data.meetingId].members);
                     socketIO.to(joinMeetingSocket[data.meetingId].members[i].id).emit('allowOneTrue')
                 }
                 joinMeetingSocket[data.meetingId].members = [];
