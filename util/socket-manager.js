3c5294eb7405ed86b858716e09bd4dd07d9cf6a1
brijesh165 <brijesh16591@gmail.com>
2022-02-03 17:09:13 +0530

    Fix

--- util/socket-manager.js
+++ util/socket-manager.js
@@ -205,9 +205,10 @@ exports.openIO = function (io) {
             socket.socketId = socket.id;
             joinMeetingSocket[meetingId].members.push({ id: socket.id, name: username, allowed: true });
             const UniqueIdMembers = joinMeetingSocket[socket.meetingId].members.length > 0 && joinMeetingSocket[socket.meetingId].members.filter((item) => item.id !== socket.id);
-            if (UniqueIdMembers) {
-                joinMeetingSocket[socket.meetingId].members = UniqueIdMembers;
-            }
+            // if (UniqueIdMembers) {
+            //     joinMeetingSocket[socket.meetingId].members = UniqueIdMembers;
+            // }
+            console.log('UniqueIdMembers :', UniqueIdMembers);
         })
 
         socket.on("disconnect", async () => {
